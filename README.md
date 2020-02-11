00d5fb4bcb9076011a71e95cf9591400c1741f6c
Anderson Luís Silva <andersonluis87@gmail.com>
2020-02-11 01:39:51 -0300

    Estrutura pronta e projeto configurado...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fast-feet
+# Fast Feet
 This is an app that has been developed during the Rocketseat's course.
---
+++ src/database/migrations/20200211043020-create-recipients.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rua: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      complemento: {
+        type: Sequelize.STRING,
+      },
+      estado: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cidade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down: queryInterface => queryInterface.dropTable('recipients'),
+};
